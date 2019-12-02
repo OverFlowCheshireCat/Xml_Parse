e452240b3cb86cef3b5aef4723b2022bf09f10ca
dit03 <dit@imac-4.local>
2019-12-02 16:20:03 +0900

    Finish

--- Xml_Parse/ViewController.swift
+++ Xml_Parse/ViewController.swift
@@ -1,20 +1,74 @@
-//
-//  ViewController.swift
-//  Xml_Parse
-//
-//  Created by dit03 on 2019. 12. 2..
-//  Copyright © 2019년 201730529. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, XMLParserDelegate {
 
+    var item: [String : String] = [:]
+    
+    var itemList: [[String : String]] = []
+    
+    var currentElement = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        /*let path = Bundle.main.url(forResource: "Fruit", withExtension: "xml") // .xml 파일 형식 파싱
+        
+        if path != nil {
+            let parser = XMLParser(contentsOf: path!)
+            
+            if parser != nil {
+                parser?.delegate = self
+                
+                if (parser?.parse())! {
+                    print(itemList)
+                } else {
+                    print("Parse Error")
+                }
+            } else {
+                print("Parser Nil Pointer Error")
+            }
+        } else {
+            print("Not Found File Error")
+        }*/
+        
+        let url = "http://api.androidhive.info/pizza/?format=xml" // xml 웹 형식 파싱
+        
+        let path = NSURL(string: url)
+        
+        if path != nil {
+            let parser = XMLParser(contentsOf: path! as URL)
+            
+            if parser != nil {
+                parser?.delegate = self
+                
+                if (parser?.parse())! {
+                    print(itemList)
+                } else {
+                    print("Parse Error")
+                }
+            } else {
+                print("Parser Nil Pointer Error")
+            }
+        } else {
+            print("Not Found File Error")
+        }
     }
-
-
+    
+    public func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        currentElement = elementName
+    }
+    
+    public func parser(_ parser: XMLParser, foundCharacters string: String) {
+        let data = string.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines)
+        
+        if !data.isEmpty {
+            item[currentElement] = data
+        }
+    }
+    
+    public func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        if elementName == "item" {
+            itemList.append(item)
+        }
+    }
 }
-
